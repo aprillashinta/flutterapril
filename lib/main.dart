3abd217bf79026a0bec25bc12975ec0edfd9eab0
Aprilla Shinta <aprillaaddivi@gmail.com>
2026-03-17 23:24:32 +0700

    dart 2

--- lib/main.dart
+++ lib/main.dart
@@ -6,62 +6,50 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Profil Mahasiswa UDB',
+      title: 'FIKOM UDB',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Profil Saya'),
+          title: const Text('Profil Mahasiswa Yudebe'),
           backgroundColor: Colors.blue,
-          centerTitle: true,
         ),
-        body: Center(
+        body: const Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              // Foto profil berbentuk lingkaran
-              const CircleAvatar(
-                radius: 60,
-                backgroundColor: Colors.blue,
-                backgroundImage: AssetImage('assets/poto1.png'),
+              Image(image: AssetImage('assets/poto1.png'), width: 150),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/helo.png'),
               ),
-              const SizedBox(height: 20), // Jarak antar elemen
-              // Nama Lengkap
-              const Text(
+              Icon(Icons.account_circle, size: 100, color: Colors.blue),
+              Text(
                 'Aprilla Shinta',
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-              ),
 
-              // Detail Jurusan
-              const Text(
-                'S1 Sistem Informasi',
-                style: TextStyle(fontSize: 18, color: Colors.grey),
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              const Text(
-                'Universitas Duta Bangsa Surakarta',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 20),
-
-              // Baris untuk Email
-              const Row(
+              Text('Quality Assurance & Analyst UI/UX'),
+              SizedBox(height: 20),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Icon(Icons.email, color: Colors.blue),
+                  Icon(Icons.email),
                   SizedBox(width: 10),
-                  Text('aprillashinta@student.udb.ac.id'),
+
+                  Text('aprillaaddivi@gmail.com'),
                 ],
               ),
             ],
           ),
         ),
       ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
